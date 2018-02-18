f226f1b37bbebe0079806710c059d8229c39ce4a
Sebastien Vasylets <sebastien.vasylets@artcom-venture.de>
2018-02-18 20:40:48 +0200

    maybe it will help

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 // @ts-nocheck
 var TelegramBot = require('node-telegram-bot-api');
 
